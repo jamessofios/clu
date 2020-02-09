0be1b80448f2762ec573b33e9bd04e6e3e898fe9
James Sofios <jamessofios@gmail.com>
2020-02-09 10:47:43 -0600

    Added comments and added to the output.

--- main.c
+++ main.c
@@ -6,7 +6,9 @@
 int main(void){
 	char* c = pwd();
 	puts(c);
+	puts("____");
 	int s = ls();
-	printf("%d\n", s);
+	puts("____");
+	printf("Total: %d\n", s);
 	return 0;
 }
--- nav.h
+++ nav.h
@@ -2,11 +2,13 @@
 #define _GNU_SOURCE
 #endif
 
+//include guard
 #ifndef NAV_H
 #define NAV_H
 
+//functions
 int ls(void);
-
 char* pwd(void);
 
 #endif
+//end include guard
